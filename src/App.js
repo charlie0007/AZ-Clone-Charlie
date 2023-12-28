727bbb9b06d16b213bdc94acfb58e43b97b4b5bc
Sehar Jiwan <seharjag@amazon.com>
2023-12-29 00:28:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,151 @@
+import "./styles.css";
+import { useState } from "react";
+import { ContinueWatching } from "./util";
+
+export default function App() {
+  const [showHomeOption, setHomeOption] = useState(false);
+  const [showCategoryOption, setCategoryOption] = useState(false);
+  const onHomeClickHandler = () => {
+    setHomeOption((prev) => !prev);
+  };
+
+  const onCategoryClickHandler = () => {
+    setCategoryOption((prev) => !prev);
+  };
+
+  return (
+    <div className="App">
+      <nav className="header-nav">
+        <h4>prime video</h4>
+        <div
+          className={`home${showHomeOption ? " back" : ""}`}
+          onClick={onHomeClickHandler}
+        >
+          <p>
+            Home <i class="fa-solid fa-caret-down"></i>
+          </p>
+          {showHomeOption && (
+            <div class={`home-options${showHomeOption ? " back" : ""}`}>
+              <span>All</span>
+              <span>Movies</span>
+              <span>TV Shows</span>
+            </div>
+          )}
+        </div>
+        <p>Store</p>
+        <p>Live TV</p>
+        <div onClick={onCategoryClickHandler}>
+          <p>
+            Categories <i class="fa-solid fa-caret-down"></i>
+          </p>
+        </div>
+        <p>My Stuff</p>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <div className="profile-image"></div>
+      </nav>
+      {showCategoryOption && (
+        <div className="categories-list">
+          <div className="categories-list_heading">
+            <div>
+              <p>Genres</p>
+            </div>
+            <ul>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+            </ul>
+          </div>
+          <div className="categories-list_heading">
+            <div>
+              <p>Featured Collection</p>
+            </div>
+            <ul>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+              <li>Action and adventure</li>
+            </ul>
+          </div>
+        </div>
+      )}
+      <div className="main-images">
+        <ul>
+          <li>
+            <div>
+              <img
+                alt="Dry Day"
+                src="https://m.media-amazon.com/images/S/sonata-images-prod/PV_IN_DryDay/2446cce1-703a-4242-a0d3-9eff615b86f6._UR3840,1440_SX2880_FMjpg_.jpeg"
+              />
+            </div>
+            <section className="main-images-title">
+              <h3>#1 in India</h3>
+              <img
+                alt="Dry Day"
+                src="https://m.media-amazon.com/images/S/sonata-images-prod/PV_IN_DryDay/07045e85-add0-43cf-9922-e664b5cf5e15._AC_SX1000_FMpng_.png"
+              />
+              <h3>Included with prime</h3>
+              <div className="main-controls">
+                <i class="fa-solid fa-play"></i>
+                <span>Play</span>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-circle-info"></i>
+              </div>
+            </section>
+          </li>
+          <li>
+            <div>
+              {/* <video
+                controls
+                src="https://s3.ll.videorolls.row.aiv-cdn.net/ww_iad/7cc0/ba1a/4752/4f00-859f-3e653bb3c359/c85d0241-c2b3-4531-b129-4e94e15e96ef_video_720p_2500kbps_audio_aaclc_128kbps.mp4"
+                data-testid="preroll-player"
+                preload="auto"
+              ></video> */}
+              <img
+                alt="Dry Day"
+                src="https://m.media-amazon.com/images/S/sonata-images-prod/PV_IN_Flames_S4/3b2b49dd-650b-41db-86ef-4ffd7006d62b._UR3840,1440_SX2880_FMjpg_.jpeg"
+              />
+            </div>
+            <section className="main-images-title">
+              <h3>#1 in India</h3>
+              <img
+                alt="Dry Day"
+                src="https://m.media-amazon.com/images/S/sonata-images-prod/PV_IN_DryDay/07045e85-add0-43cf-9922-e664b5cf5e15._AC_SX1000_FMpng_.png"
+              />
+              <h3>Included with prime</h3>
+              <div className="main-controls">
+                <i class="fa-solid fa-play"></i>
+                <span>Play</span>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-circle-info"></i>
+              </div>
+            </section>
+          </li>
+        </ul>
+      </div>
+      <div className="continue-container">
+        {ContinueWatching.map((picture) => (
+          <picture>
+            <source
+              type={picture.source1.type}
+              srcset={picture.source1.srcset}
+              sizes={picture.source1.sizes}
+            ></source>
+            <source
+              type={picture.source2.type}
+              srcset={picture.source2.srcset}
+              sizes={picture.source2.sizes}
+            ></source>
+            <img
+              alt=""
+              style={{ "aspect-ratio": 16 / 9 }}
+              src={picture.image.src}
+              data-testid={picture.image["data-testid"]}
+              loading={picture.image.loading}
+            />
+          </picture>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/util.js
@@ -0,0 +1,186 @@
+export const ContinueWatching = [
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX360_FMwebp_.png 360w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX480_FMwebp_.png 480w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX720_FMwebp_.png 720w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX1080_FMwebp_.png 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/png",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX360_FMpng_.png 360w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX480_FMpng_.png 480w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX720_FMpng_.png 720w, https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX1080_FMpng_.png 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/c8000bc2d5e58cd76bbf945907cb3f8afa114972e46f4957e28a7136666813af._UR1920,1080_SX720_FMpng_.png",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX360_FMwebp_.jpg 360w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX480_FMwebp_.jpg 480w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX720_FMwebp_.jpg 720w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX360_FMjpg_.jpg 360w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX480_FMjpg_.jpg 480w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX720_FMjpg_.jpg 720w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b15d7da4c9039a532da0a1c0d988f95eb37c113c679fecf187943bf5c1cb4662._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX360_FMwebp_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX480_FMwebp_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX720_FMwebp_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX360_FMjpg_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX480_FMjpg_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX720_FMjpg_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/32d817cc3cb7d48e6e5e68b388e04f4fab6084a24d2b3840105d70cfed27c4a7._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX360_FMwebp_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX480_FMwebp_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX720_FMwebp_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX360_FMjpg_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX480_FMjpg_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX720_FMjpg_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/94b522817fbe019825ec6d71a12353366645dda4122dfe89dfdd91f22ce7b22d._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX360_FMwebp_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX480_FMwebp_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX720_FMwebp_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX360_FMjpg_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX480_FMjpg_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX720_FMjpg_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/e97ec15388c11fa23d2ad473884dfe0c5fc5e7fe2d48d18e41999747611b4716._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX360_FMwebp_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX480_FMwebp_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX720_FMwebp_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX360_FMjpg_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX480_FMjpg_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX720_FMjpg_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/2afb6df4e3f1d5420d1c632364e0a2c4698e9124969199f613efa39332d59617._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX360_FMwebp_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX480_FMwebp_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX720_FMwebp_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX1080_FMwebp_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/jpeg",
+      srcset:
+        "https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX360_FMjpg_.jpg 360w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX480_FMjpg_.jpg 480w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX720_FMjpg_.jpg 720w, https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX1080_FMjpg_.jpg 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://m.media-amazon.com/images/S/pv-target-images/6d2d29ce5108760a00732e3bbd9219506d91133cc78fe723bd0bed29efcea1b7._UR1920,1080_SX720_FMjpg_.jpg",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+  {
+    source1: {
+      type: "image/webp",
+      srcset:
+        "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX360_FMwebp_.png 360w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX480_FMwebp_.png 480w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX720_FMwebp_.png 720w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX1080_FMwebp_.png 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    source2: {
+      type: "image/png",
+      srcset:
+        "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX360_FMpng_.png 360w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX480_FMpng_.png 480w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX720_FMpng_.png 720w, https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX1080_FMpng_.png 1080w",
+      sizes:
+        "(max-width: 28em) 260px, (max-width: 55em) 260px, (max-width: 80em) 390px, (max-width: 100em) 390px, (max-width: 150em) 540px, 540px",
+    },
+    image: {
+      alt: "",
+      style: "aspect-ratio:16/9",
+      src: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3526223bd90b5b658180eb733104cdd83e2e1a113577b8cb3242524910f0cdc5._UR1920,1080_SX720_FMpng_.png",
+      "data-testid": "base-image",
+      loading: "eager",
+    },
+  },
+];
